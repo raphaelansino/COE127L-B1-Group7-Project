fe8755d268c9f61f10921bffefc6520c51dfa922
AndujarBryan <52277812+AndujarBryan@users.noreply.github.com>
2019-07-06 00:35:47 +0800

    Andujar Commits

--- main.cpp
+++ main.cpp
@@ -1,26 +1,45 @@
 #include <iostream>
-#include <player.h>
-#include <game.h>
+#include <omp.h>
+#include <stdlib.h>
+#include "HashTable.h"
 
 using namespace std;
 
-int main
+#define FILENAME_LEN 50
+#define FILENAME_NUM 100
+#define HASH_TABLE_MAX_SIZE 100000
+#define MAX_LINE_SIZE 100000
+
+char text[FILENAME_NUM][MAX_LINE_SIZE * 50];
+int hash_size[FILENAME_NUM];
+typedef struct HashTable Node;
+
+int main()
 {
-std::string name1;
-std::string name2;
-std::string str;
-int score1;
-int score2;
+	int file_num = 0;
+	FILE *read_filename = fopen("filename_list.txt", "r");
+	char **filename_list_array = (char **)malloc(sizeof(char*)* FILENAME_NUM);
+	int i,j;
+	Node* pHead;
 
-std::getline(ifs,name1,';');
-std::getline(ifs,str,'\n');
-score1 = atoi(str.c_str());
+#pragma omp parallel for
+	for (i = 0; i < FILENAME_NUM; i++)
+	{
+		filename_list_array[i] = (char *)malloc(sizeof(char)* FILENAME_LEN);
+	}
 
-std::getline(ifs,name2,';');
-std::getline(ifs,str,'\n');
-score2 = atoi(str.c_str());
+	if (read_filename == NULL)
+	{
+		cout << "open the file incorrectly !";
+		return 0;
+	}
+	while (!feof(read_filename))
+	{
+		fscanf(read_filename, "%s\n", filename_list_array[file_num]);
+		file_num++;
+	}
+		cout << "The result can be found in output.txt .";
 
-game.play(name1,score1,name2,score2);
-
-return 0;
+	omp_set_num_threads(8);
+double time = -omp_get_wtime();
 }
