b26ebd9a8f613a2f9712b1a9296ed2847efcb1dc
Raphael Ansino <50911495+raphaelansino@users.noreply.github.com>
2019-07-26 22:33:44 +0800

    Without using Parallelization using OpenMP

---
+++ Hashing.cpp
@@ -0,0 +1,220 @@
+#include "Hashing.h"
+#include <iostream>
+#include <cstring>
+#include <chrono>
+
+using namespace std;
+
+
+#define FILENAME_LEN 50
+#define FILENAME_NUM 100
+#define HASH_TABLE_MAX_SIZE 100000
+#define MAX_LINE_SIZE 100000
+
+char text[FILENAME_NUM][MAX_LINE_SIZE * 50];
+int hash_size[FILENAME_NUM];
+typedef struct HashTable Node;
+struct HashTable
+{
+	char* Key;
+	int Value;
+	Node* pNext;
+};
+
+Hashing::Hashing()
+{
+}
+
+Hashing::~Hashing()
+{
+    //dtor
+}
+void Hashing::rmPunct2LowerFunc(char *p)
+{
+	char *src = p, *dst = p;
+	while (*src)
+	{
+		if (ispunct((unsigned char)*src) || isdigit((unsigned char)*src))
+		{
+			src++;
+		}
+		else if (isupper((unsigned char)*src))
+		{
+			*dst++ = tolower((unsigned char)*src);
+			src++;
+		}
+		else if (src == dst)
+		{
+			src++;
+			dst++;
+		}
+		else
+		{
+			*dst++ = *src++;
+		}
+	}
+	*dst = 0;
+}
+
+void Hashing::readFunc(const char * filename, int index)
+{
+
+    FILE *fp = fopen(filename, "r");
+	char word[1000];
+	if (fp == NULL)
+	{
+		cout << "input file is invalid !" << endl;
+		return;
+	}
+
+	while (fscanf(fp, " %s", word) == 1)
+	{
+		rmPunct2LowerFunc(word);
+		strcat(text[index], word);
+		strcat(text[index], " ");
+	}
+	fclose(fp);
+}
+Node * hashTable[FILENAME_NUM + 1][HASH_TABLE_MAX_SIZE];
+unsigned int Hashing::hashComputeByHashFunc(const char* key)
+{
+	unsigned int hash = 1;
+	int temp;
+	while (temp = *key++)
+	{
+		hash = hash * 33 + temp;
+	}
+	return hash;
+}
+
+
+Node* Hashing::hashTableLookupFunc(const char* key, int index)
+{
+	unsigned int pos = hashComputeByHashFunc(key) % HASH_TABLE_MAX_SIZE;
+	Node* pHead;
+	if (hashTable[index][pos])
+	{
+		pHead = hashTable[index][pos];
+		while (pHead)
+		{
+			if (strcmp(key, pHead->Key) == 0)
+			{
+				return pHead;
+			}
+			pHead = pHead->pNext;
+		}
+	}
+	return NULL;
+}
+
+void Hashing::hashTableInsertFunc(const char* key, int value, int index)
+{
+	unsigned int pos, poscheck;
+	Node* NewNode = (Node*)malloc(sizeof(Node));
+	memset(NewNode, 0, sizeof(Node));
+	NewNode->Key = (char*)malloc(sizeof(char)* (strlen(key) + 1));
+	strcpy(NewNode->Key, key);
+	NewNode->Value = value;
+
+	pos = hashComputeByHashFunc(key) % HASH_TABLE_MAX_SIZE;
+	poscheck = pos;
+
+	while (hashTable[index][poscheck] != NULL && hashTable[index][poscheck]->Value != -1)
+	{
+		poscheck++;
+		poscheck %= HASH_TABLE_MAX_SIZE;
+	}
+	NewNode->pNext = hashTable[index][pos];
+	hashTable[index][pos] = NewNode;
+
+	hash_size[index]++;
+}
+
+void Hashing::mapperFunc(int index)
+{
+    char *nextWord;
+	nextWord = strtok(text[index], " \r\n");
+	while (nextWord != NULL)
+	{
+		{
+			if (hashTableLookupFunc(nextWord, index) == NULL)
+			{
+				hashTableInsertFunc(nextWord, 1, index);
+			}
+			else
+			{
+				hashTableLookupFunc(nextWord, index)->Value++;
+			}
+			nextWord = strtok(NULL, " \r\n");
+		}
+	}
+}
+
+void Hashing::reducerFunc(int reduceCount)
+{
+	int i, j;
+	for (i = 0; i < reduceCount; ++i)
+	{
+		for (j = 0; j < HASH_TABLE_MAX_SIZE; j++)
+		{
+			if (hashTable[i][j])
+			{
+				Node* pHead = hashTable[i][j];
+				while (pHead)
+				{
+					if (hashTableLookupFunc(pHead->Key, FILENAME_NUM) == NULL)
+						hashTableInsertFunc(pHead->Key, pHead->Value, FILENAME_NUM);
+					else
+					{
+						int val = pHead->Value;
+						hashTableLookupFunc(pHead->Key, FILENAME_NUM)->Value += val;
+					}
+					pHead = pHead->pNext;
+				}
+			}
+		}
+	}
+}
+
+void Hashing::writerFunc(FILE * fp)
+{
+	int i;
+	Node* p;
+	fprintf(fp, "------Word Count Results------ \n------------------------------\n");
+	{
+		for (i = 0; i < HASH_TABLE_MAX_SIZE; ++i)
+		{
+			if (hashTable[FILENAME_NUM][i] != NULL)
+			{
+				p = hashTable[FILENAME_NUM][i];
+				while (p)
+				{
+					fprintf(fp, "Word: %s, Count: %d\n", p->Key, p->Value);
+					p = p->pNext;
+
+				}
+			}
+		}
+	}
+}
+
+void Hashing::hashTablePrintFunc(int index)
+{
+	int i = 0;
+	Node* pHead;
+	printf("=========== content of hash table ===========\n");
+	while (i < HASH_TABLE_MAX_SIZE)
+	{
+		if (hashTable[index][i])
+		{
+			pHead = hashTable[index][i];
+			while (pHead)
+			{
+				printf("Word:  %s  ,  count: %d ", pHead->Key, pHead->Value);
+				pHead = pHead->pNext;
+				printf("\n");
+			}
+		}
+		i++;
+	}
+}
--- Hashing.h
+++ Hashing.h
@@ -1,21 +1,31 @@
-#pragma once
+#ifndef HASHING_H
+#define HASHING_H
+#include <cstdio>
+
 
 class Hashing
 {
-public:
-	Hashing();
-	~Hashing();
+    public:
+        Hashing();
+        virtual ~Hashing();
 
-	typedef struct HashTable Node;
 
-	unsigned int hashComputeByHashFunc(const char* key);
-	void hashTableInsertFunc(const char* key, int value, int index);
-	Node* hashTableLookupFunc(const char* key, int index);
-	void hashTablePrintFunc(int index);
-	void rmPunct2LowerFunc(char * p);
-	void readFunc(const char* filename, int index);
-	void mapperFunc(int index);
-	void reducerFunc(int reduceCount);
-	void writerFunc(FILE * fp);
+        typedef struct HashTable Node;
+        void readFunc(const char* filename, int index );
+        void rmPunct2LowerFunc(char *p);
+        void mapperFunc(int index);
+        unsigned int hashComputeByHashFunc(const char* key);
+        void hashTableInsertFunc(const char* key, int value, int index);
+        Node* hashTableLookupFunc(const char* key, int index);
+        void reducerFunc(int reduceCount);
+        void writerFunc(FILE* fp);
+        void hashTablePrintFunc(int index);
+
+
+
+
 };
+
+#endif // HASHING_H
+
 
